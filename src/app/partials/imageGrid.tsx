1865be6f64ad738afd2c4f4e8f1e0124a6b2aa82
Silvestre Esteban Fernandez Linero <silvestre.fernandez@sebdata.com>
2025-03-06 19:32:27 +1300

    remove bg-black from imagegrid

--- src/app/partials/imageGrid.tsx
+++ src/app/partials/imageGrid.tsx
@@ -8,7 +8,7 @@ import leatherJacket from "../images/leatherjacket.jpg";
 
 export default function ImageGrid() {
     return (
-        <div className="mx-auto grid grid-cols-4 grid-rows-2 gap-2 bg-black p-2 h-[calc(100vh-60px)] max-h-[1300px]" style={{width: "75%"}}>
+        <div className="mx-auto grid grid-cols-4 grid-rows-2 gap-2 p-2 h-[calc(100vh-60px)] max-h-[1300px]" style={{width: "75%"}}>
             <div className="col-span-1 row-span-2">
                 <Image src={leatherJacket} alt="leather jacket pic" />
                 <Image src={selfie} alt="Selfie" className="mt-4" />
